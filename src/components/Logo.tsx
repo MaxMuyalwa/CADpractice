f201d759114cceb7305ad144b4897e45db9adf77
Max Muyalwa <mmkaluku@gmail.com>
2026-03-27 08:34:20 +0200

    fix(Logo): Correct SVG indentation and formatting
    Improves the readability of the Logo component's SVG code by adjusting indentation and ensuring consistent formatting. This change does not affect the visual output of the logo.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 export const Logo = ({ className = "w-8 h-8" }: { className?: string }) => {
   return (
-    <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120.68 139.35" className={className}>
+    <svg 
+      id="Layer_2" 
+      data-name="Layer 2" 
+      xmlns="http://www.w3.org/2000/svg" 
+      viewBox="0 0 120.68 139.35"
+      className={className}
+    >
       <g id="Layer_1-2" data-name="Layer 1">
         <g>
           <g>
